91bc46acfdb3bbc281669cc5cf957139222ec893
Roberto1501 <robertocarvalho1501@yahoo.com>
2023-08-22 18:07:52 -0300

    Recomeçando o projeto final banco de dados 2

--- src/data/users.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../models/user";
-
-export const usersDb = [
-  new User("lisa", "lisa@teste.com", "2030"),
-  new User("josé", "jose@teste.com", "5698"),
-  new User("antonio", "antonio@teste.com", "5412"),
- 
-];
